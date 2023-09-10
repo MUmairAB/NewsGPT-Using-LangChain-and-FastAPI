717b63d7c82563faa4253170e962d30a3faa39ca
Umair Akram <120496694+MUmairAB@users.noreply.github.com>
2023-09-10 14:10:05 +0500

    Bash command to use the NewsGPT from GitBash

---
+++ bash_code.sh
@@ -0,0 +1,5 @@
+# Use the following code to access the NewsGPT app from GitBash.
+#  Replace the URL parameter with your desired news article's url and also update the initial end-point address.
+
+$ curl -X 'GET' \ 
+https://ea78-35-194-207-7.ngrok.io/summary?URL=https://www.theguardian.com/world/2023/sep/10/chinas-good-for-marriage-womens-trend-ignites-social-media-debate
